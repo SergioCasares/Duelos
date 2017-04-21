d993e9e837d3308bb6da3ce6173f417e5b06ccdf
DJBiokinetix <DJBiokinetix@DESKTOP-LKG1B3B>
2017-04-20 19:16:27 -0500

    Nueva modificacion de plugin.yml y TPHCommand.java

---
+++ PlanetCraft-Duels/src/me/djbiokinetix/comandos/admin/TPHCommand.java
@@ -0,0 +1,61 @@
+package me.djbiokinetix.comandos.admin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import me.djbiokinetix.Main;
+
+public class TPHCommand implements CommandExecutor {
+
+	public Main main;
+	
+	public TPHCommand(Main instancia) {
+		
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if (sender instanceof Player) {
+			
+			Player p = (Player) sender;
+			
+			if (args.length == 0) {
+				p.sendMessage(main.c("&8[&6Code&8] &7Uso correcto: &c/tp <Player>"));
+			}
+			
+			if (args.length > 0) {
+
+					
+				Player target = Bukkit.getPlayer(args[0]);
+				
+				if (target == p) {
+					p.sendMessage(main.c("&8[&6Code&8] &7No puedes teletransportarte a ti mismo."));
+					return true;
+				}
+				
+				if (target == null) {
+					p.sendMessage(main.c("&8[&6Code&8] &7Ese jugador esta offline!"));
+					return true;
+				}
+				
+				if (p.hasPermission("planetcraft-duels.teleport.tp")) {
+					target.teleport(p);
+					target.sendMessage(main.c("&8[&6Code&8] &7Teletransportando con &b"+p.getName()));
+					return false;
+				} else {
+					p.sendMessage(main.c("&8[&6Code&8] &7No tienes permisos."));
+					return true;
+				}
+				
+			}
+			
+		} else {
+			return true;
+		}
+		return false;
+	}
+
+}
